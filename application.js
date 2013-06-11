eb2c84ad66ef20d0bf4b814d9b0b5d30580a3648
Deepak <deepak.coral@gmail.com>
2013-06-11 06:08:53 +0530

    changed arrow positioning

--- application.js
+++ application.js
@@ -4,25 +4,31 @@ $(document).ready(function(){
 	$(".box1").hover(function(){
 		$('.originalmessage').hide();
 		$('.boxleft').show();
+		$('#arrowleft').css('display','block');
 	}, function(){
 		$('.boxleft').hide();
 		$('.originalmessage').show();
+		$('#arrowleft').css('display','none');
 	});
 
 	$(".box2").hover(function(){
 		$('.originalmessage').hide();
 		$('.boxcenter').show();
+		$('#arrowcenter').css('display','block');
 	}, function(){
 		$('.boxcenter').hide();
 		$('.originalmessage').show();
+		$('#arrowcenter').css('display','none');
 	});
 
 	$(".box3").hover(function(){
 		$('.originalmessage').hide();
 		$('.boxright').show();
+		$('#arrowright').css('display','block');
 	}, function(){
 		$('.boxright').hide();
 		$('.originalmessage').show();
+		$('#arrowright').css('display','none');
 	});
 
 });
